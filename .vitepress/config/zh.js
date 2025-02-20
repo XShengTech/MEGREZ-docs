e851ad24e7510ea43d8e8dfeaa96dbb20f36ff47
Harry-zklcdc <zklcdc@qq.com>
2025-02-20 16:56:09 +0800

    [Docs] 📝 Updata Link

--- .vitepress/config/zh.js
+++ .vitepress/config/zh.js
@@ -74,7 +74,7 @@ export const zh = defineConfig({
           link: '/guide/maintenance/instances/',
           items: [
             { text: '实例状态', link: '/guide/maintenance/instances/status' },
-            { text: '实例操作', link: '/guide/maintenance/instances/actions' },
+            { text: '实例操作', link: '/guide/maintenance/instances/operate' },
             { text: '修改配置', link: '/guide/maintenance/instances/resume' },
             { text: '删除实例', link: '/guide/maintenance/instances/delete' },
           ]
@@ -84,8 +84,8 @@ export const zh = defineConfig({
           collapsed: false,
           link: '/guide/maintenance/users/',
           items: [
-            { text: '用户权限', link: '/guide/maintenance/users/status' },
-            { text: '修改密码', link: '/guide/maintenance/users/status' },
+            { text: '用户权限', link: '/guide/maintenance/users/permission' },
+            { text: '用户设置', link: '/guide/maintenance/users/settings' },
             { text: '删除用户', link: '/guide/maintenance/users/delete' },
           ]
         },
